fb78407b8581151594349b9e6e44ed0ba6de4d88
Sugeng Winanjuar <winanjuar@gmail.com>
2023-09-16 14:23:38 +0700

    add h2 db

---
+++ src/main/java/sdw/springacademy/cashcard/CashCardRepository.java
@@ -0,0 +1,7 @@
+package sdw.springacademy.cashcard;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+
+}
